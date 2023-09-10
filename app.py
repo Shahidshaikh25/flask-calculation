62234bbdbd43086c952fa00d637865db31b6ef3f
shahid shaikh <shahidshaikh258200110@gmail.com>
2023-09-10 19:00:52 +0530

    flask rev

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask,render_template,request
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "welcome to the app"
+
+@app.route('/cal',methods=["GET"])
+def math_operator():
+    operation=request.json["operation"]
+    number1=request.json["number1"]
+    number2=request.json["number2"]
+    
+    if operation=="add":
+        result=int(number1)+int(number2)
+    elif operation=="multiply":
+        result=int(number1)*int(number2)
+    elif operation=="division":
+        result=int(number1)/int(number2)
+    else:
+        result=int(number1)-int(number2)
+    return "the operation is {} and the result is {}".format(operation,result)
+
+if __name__=='__main__':
+    app.run(debug=True)
